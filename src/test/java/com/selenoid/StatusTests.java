95e80b4dbfac6890fea4f7c321822c2e5a225c50
Alina <alinavol30@gmail.com>
2023-12-17 22:05:45 +0300

    specs

--- src/test/java/com/selenoid/StatusTests.java
+++ src/test/java/com/selenoid/StatusTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
@@ -83,6 +84,23 @@ public class StatusTests {
                 .log().status()
                 .log().body()
                 .statusCode(200)
+                .body("total", is(20))
+                .body("browsers.chrome", hasKey("100.0"));
+    }
+
+    @Test
+    void checkTotalWithJsonSchema() {
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("schemas/status-response-schema.json"))
                 .body("total", is(20))
                 .body("browsers.chrome", hasKey("100.0"));
     }
---
+++ src/test/java/in/reqres/specs/LoginSpec.java
@@ -0,0 +1,38 @@
+package in.reqres.specs;
+
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static in.reqres.helpers.CustomAllureListener.withCustomTemplates;
+import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
+import static io.restassured.http.ContentType.JSON;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+public class LoginSpec {
+    public static RequestSpecification loginRequestSpec = with()
+            .log().uri()
+            .log().method()
+            .log().body()
+            .filter(withCustomTemplates())
+            .contentType(JSON)
+            .baseUri("https://reqres.in")
+            .basePath("/api");
+
+    public static ResponseSpecification loginResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(200)
+            .expectBody(matchesJsonSchemaInClasspath("schemas/success-login-response-schema.json"))
+            .build();
+
+    public static ResponseSpecification missingPassword400Spec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(400)
+            .expectBody("error", notNullValue()) // todo move to schema validation
+            .build();
+}
